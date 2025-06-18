ef5c56361ec95c30c8a7a452c80bd8e475499ebc
Mohamed Amine KERKOURI <kerkourima@gmail.com>
2025-06-18 11:21:31 +0200

    Update launch-vllm-server.sh

--- dataset-regenerate/launch-vllm-server.sh
+++ dataset-regenerate/launch-vllm-server.sh
@@ -1,11 +1,11 @@
 
 # Serve the model
-CUDA_VISIBLE_DEVICES=0,1 vllm serve unsloth/Llama-3.3-70B-Instruct-bnb-4bit \
+CUDA_VISIBLE_DEVICES=0,1 vllm serve unsloth/QwQ-32B-unsloth-bnb-4bit \
   --host 0.0.0.0  \
   --port  8000 \
   --tensor-parallel-size 2 \
   --max-model-len 50000 \
-  --api-key key \
+  --api-key API-KEY \
   --quantization=bitsandbytes \
   --load-format=bitsandbytes \
   --gpu-memory-utilization=0.75 \
